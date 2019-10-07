fee56ab71468eba69fd09a0000e79dd51e444011
MargoPro <54171360+MargoPro@users.noreply.github.com>
2019-10-07 13:10:32 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+// to the Top Button
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#scroll').fadeIn();
+        }else{
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
+
+// Hide menu on scroll
+
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-59px";
+  }
+  prevScrollpos = currentScrollPos;
+}
